db320f213789db5fc1305f1fbfab14a5c9ede93f
Uka0001 <ukraine0001@gmail.com>
2022-08-09 20:29:41 +0300

    switch type resolving

---
+++ src/main/java/com/knubisoft/ORM/FileContentTypeEnum.java
@@ -0,0 +1,8 @@
+package com.knubisoft.ORM;
+
+public enum FileContentTypeEnum {
+    CSV,
+    XML,
+    JSON,
+    TXT
+}
--- src/main/java/com/knubisoft/ORM/Main.java
+++ src/main/java/com/knubisoft/ORM/Main.java
@@ -6,10 +6,25 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static com.knubisoft.ORM.FileContentTypeEnum.*;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        InputStream stream = Main.class.getClassLoader().getResourceAsStream("sample.txt");
+        String inputFile = "sample.txt";
+        switch (inputFile):{
+            case (CSV):
+                break;
+            case (XML):
+                break;
+            case (JSON):
+                InputStream stream = Main.class.getClassLoader().getResourceAsStream("sample.JSON");
+                break;
+            case (TXT):
+                InputStream stream = Main.class.getClassLoader().getResourceAsStream(inputFile);
+                break;
+        }
+
         List<String> lines = IOUtils.readLines(stream, StandardCharsets.UTF_8);
         List<Person> personList = CsvORM.transform(lines);
     }
