07dddb7b6d3524804b0a9d7654eef28b9636955c
Uka0001 <ukraine0001@gmail.com>
2022-08-09 20:42:52 +0300

    Apache Tika detecting tool try to implement

--- src/main/java/com/knubisoft/ORM/Main.java
+++ src/main/java/com/knubisoft/ORM/Main.java
@@ -1,14 +1,21 @@
 package com.knubisoft.ORM;
 
 import org.apache.commons.io.IOUtils;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import javax.activation.MimeType;
+import org.apache.tika.Tika;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        File inputFile = new File("sample.txt");
+        String type = new Tika().detect(inputFile);
+        System.out.println(type);
         InputStream stream = Main.class.getClassLoader().getResourceAsStream("sample.txt");
         List<String> lines = IOUtils.readLines(stream, StandardCharsets.UTF_8);
         List<Person> personList = CsvORM.transform(lines);
