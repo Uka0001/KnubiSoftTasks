37bbee74c23441ba73d6cccbfdadb081a1f98cc5
Uka0001 <ukraine0001@gmail.com>
2022-08-22 01:32:22 +0300

    countBy metod in Kata task from codewars completed

---
+++ src/main/java/com/knubisoft/tasks/codewars/Kata8.java
@@ -0,0 +1,29 @@
+package com.knubisoft.tasks.codewars;
+
+public class Kata8{
+    public static int[] countBy(int x, int n){
+ //       if (x >= 1) {
+            int[] count = new int[n];
+            for (int i = 0; i < n; i++) {
+                count[i] = x * (i + 1);
+            }
+            return count;
+//        } else {
+//            int[] count = {x};
+//            return count;
+//        }
+    }
+    // smart decision from code wars with stream
+    // return IntStream.rangeClosed(1, n)
+    //      .map(i -> i * x)
+    //      .toArray();
+}
+//Create a function with two arguments
+// that will return an array of the first (n) multiples of (x).
+//
+//Assume both the given number and
+// the number of times to count will
+// be positive numbers greater than 0.
+//
+//Return the results as an array
+// (or list in Python, Haskell or Elixir).
