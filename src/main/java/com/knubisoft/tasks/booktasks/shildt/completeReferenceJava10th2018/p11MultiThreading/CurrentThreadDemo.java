64e874f0cd283a34d8d2ed30599b436f3a918d9e
Uka0001 <ukraine0001@gmail.com>
2022-08-23 12:02:35 +0300

    CurrentThreadDemo example from shildt book

---
+++ src/main/java/com/knubisoft/tasks/booktasks/shildt/completeReferenceJava10th2018/p11MultiThreading/CurrentThreadDemo.java
@@ -0,0 +1,22 @@
+package com.knubisoft.tasks.booktasks.shildt.completeReferenceJava10th2018.p11MultiThreading;
+
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+
+        System.out.println("Current thread" + t);
+
+        //change name of the thread
+        t.setName("My Thread");
+        System.out.println("After name changed" + t);
+
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e){
+            System.out.println("The main thread have aborted");
+        }
+    }
+}
