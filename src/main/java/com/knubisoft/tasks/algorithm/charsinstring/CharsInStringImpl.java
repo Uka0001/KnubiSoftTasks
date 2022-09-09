914a5d2a819ed5f9e2170c544b43e5e5e72b8800
Uka0001 <ukraine0001@gmail.com>
2022-09-09 10:16:42 +0300

    CharsInString algoritm task completed

--- src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
@@ -1,10 +1,21 @@
 package com.knubisoft.tasks.algorithm.charsinstring;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class CharsInStringImpl implements CharsInString {
     @Override
     public Map<Character, Integer> charactersCount(String text) {
-        return null;
+        Map<Character, Integer> map = new HashMap<>();
+        text = text.replaceAll(" ", "");
+        for (Character c : text.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return map;
     }
 }
